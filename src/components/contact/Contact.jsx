d98bca7f7dcb0d3cecee9efc3a838bc50e3b1688
klaklik developer <klaklik.developer@gmail.com>
2023-11-20 14:08:11 +0700

    fix contact me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,14 +9,17 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
-      "service_yyo7iiu",
-      "template_taxuv0p",
-      form.current,
-      "8FbbdtyhKdPA5wdvU"
-    );
+    emailjs.sendForm("service_yyo7iiu", "template_taxuv0p", form.current, "8FbbdtyhKdPA5wdvU");
     e.target.reset();
     toast.success("Email sent! Thankyou.");
+  };
+
+  const openSocialMedia = (item) => {
+    if (item === "email") {
+      window.open("https://mail.google.com/mail/u/0/#inbox?compose=DmwnWtDwjbfnQfkPKHFlpFGwCFrFcrkQCjwjmxVLhBMfflxMsptxqPHGlFPRtLwcpDJSKXwZszKQ", "_blank");
+    } else if (item === "whatsapp") {
+      window.open("https://wa.me/6281314349347?text=Hello%20there!", "_blank");
+    }
   };
 
   return (
@@ -33,13 +36,10 @@ const Contact = () => {
               <i className="bx bx-mail-send contact__card-icon"></i>
 
               <h3 className="contact__card-title">Email</h3>
-              <span className="contact__card-data">
-                adityarahman453@gmail.com
-              </span>
-              <a href="/#" className="contact__button">
-                Write me{" "}
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
-              </a>
+              <span className="contact__card-data">adityarahman453@gmail.com</span>
+              <div className="contact__button" onClick={() => openSocialMedia("email")}>
+                Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </div>
             </div>
 
             <div className="contact__card">
@@ -47,10 +47,9 @@ const Contact = () => {
 
               <h3 className="contact__card-title">Whatsapp</h3>
               <span className="contact__card-data">+6281314349347</span>
-              <a href="/#" className="contact__button">
-                Write me{" "}
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
-              </a>
+              <div className="contact__button" onClick={() => openSocialMedia("whatsapp")}>
+                Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </div>
             </div>
 
             <div className="contact__card">
@@ -58,12 +57,8 @@ const Contact = () => {
 
               <h3 className="contact__card-title">Instagram</h3>
               <span className="contact__card-data">_dityaraa.21</span>
-              <a
-                href="https://www.instagram.com/_dityaraa.21/"
-                className="contact__button"
-              >
-                Write me{" "}
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              <a href="https://www.instagram.com/_dityaraa.21/" className="contact__button">
+                Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i>
               </a>
             </div>
           </div>
@@ -75,45 +70,22 @@ const Contact = () => {
           <form ref={form} onSubmit={sendEmail} className="contact__form">
             <div className="contact__form-div">
               <label className="contact__form-tag">Name</label>
-              <input
-                type="text"
-                name="name"
-                className="contact__form-input"
-                placeholder="Insert your name"
-              />
+              <input type="text" name="name" className="contact__form-input" placeholder="Insert your name" />
             </div>
 
             <div className="contact__form-div">
               <label className="contact__form-tag">Mail</label>
-              <input
-                type="email"
-                name="email"
-                className="contact__form-input"
-                placeholder="Insert your email"
-              />
+              <input type="email" name="email" className="contact__form-input" placeholder="Insert your email" />
             </div>
 
             <div className="contact__form-div contact__form-area">
               <label className="contact__form-tag">Description</label>
-              <textarea
-                name="project"
-                cols="30"
-                rows="10"
-                className="contact__form-input"
-                placeholder="Description"
-              ></textarea>
+              <textarea name="project" cols="30" rows="10" className="contact__form-input" placeholder="Description"></textarea>
             </div>
 
             <button className="button button--flex">
               Send Message
-              <svg
-                class="button__icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
+              <svg class="button__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path
                   d="M14.2199 21.9352C13.0399 21.9352 11.3699 21.1052 10.0499 17.1352L9.32988 14.9752L7.16988 14.2552C3.20988 12.9352 2.37988 11.2652 2.37988 10.0852C2.37988 8.91525 3.20988 7.23525 7.16988 5.90525L15.6599 3.07525C17.7799 2.36525 19.5499 2.57525 20.6399 3.65525C21.7299 4.73525 21.9399 6.51525 21.2299 8.63525L18.3999 17.1252C17.0699 21.1052 15.3999 21.9352 14.2199 21.9352ZM7.63988 7.33525C4.85988 8.26525 3.86988 9.36525 3.86988 10.0852C3.86988 10.8052 4.85988 11.9052 7.63988 12.8252L10.1599 13.6652C10.3799 13.7352 10.5599 13.9152 10.6299 14.1352L11.4699 16.6552C12.3899 19.4352 13.4999 20.4252 14.2199 20.4252C14.9399 20.4252 16.0399 19.4352 16.9699 16.6552L19.7999 8.16525C20.3099 6.62525 20.2199 5.36525 19.5699 4.71525C18.9199 4.06525 17.6599 3.98525 16.1299 4.49525L7.63988 7.33525Z"
                   fill="var(--container-color)"
