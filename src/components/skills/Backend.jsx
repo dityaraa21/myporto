7a42d39f99ba074ba221537cafc681a44bdef204
klaklik developer <klaklik.developer@gmail.com>
2024-01-23 15:06:53 +0700

    change skill frontend

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,7 +3,13 @@ import { motion } from "framer-motion";
 
 const Backend = () => {
   return (
-    <motion.div initial={{ y: 100, opacity: 0 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: 0.8, delay: 0.6 }} className="skills__content">
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8, delay: 0.6 }}
+      className="skills__content"
+    >
       <h3 className="skills__title">Backend Developer</h3>
 
       <div className="skills__box">
@@ -12,7 +18,7 @@ const Backend = () => {
             <i class="bx bx-badge-check"></i>
 
             <div>
-              <h3 className="skills__name">PHP</h3>
+              <h3 className="skills__name">Laravel</h3>
               <span className="skills__level">Intermediate</span>
             </div>
           </div>
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -3,7 +3,13 @@ import { motion } from "framer-motion";
 
 const Frontend = () => {
   return (
-    <motion.div initial={{ y: 100, opacity: 0 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: 0.8, delay: 0.4 }} className="skills__content">
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8, delay: 0.4 }}
+      className="skills__content"
+    >
       <h3 className="skills__title">Frontend Developer</h3>
 
       <div className="skills__box">
@@ -60,7 +66,7 @@ const Frontend = () => {
 
             <div>
               <h3 className="skills__name">ReactJS</h3>
-              <span className="skills__level">Novice</span>
+              <span className="skills__level">Intermediate</span>
             </div>
           </div>
 
@@ -69,7 +75,7 @@ const Frontend = () => {
 
             <div>
               <h3 className="skills__name">NextJS</h3>
-              <span className="skills__level">Novice</span>
+              <span className="skills__level">Intermediate</span>
             </div>
           </div>
 
