80f5ce788c98e10b7ca277df63881f9e3f89b1d2
adit-rumat <adit.rumat@gmail.com>
2025-05-09 00:18:43 +0700

    update porto

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,60 +1,77 @@
 import Klaklik from "../../assets/COver Klaklik.png";
 import TMDB from "../../assets/tmdb-api.png";
-import Maltimart from "../../assets/maltimart.png";
 import Burane from "../../assets/burane.png";
 import Florist from "../../assets/florist.png";
 import Dashboard from "../../assets/dashboard-creator.png";
 import CreatorForum from "../../assets/creator-forum.png";
 import Kursus from "../../assets/kursus.png";
+import RumatKita from "../../assets/rumatkita.png";
+import Rosy from "../../assets/rosy-logo.png";
+import Rumatku from "../../assets/rumatku.png";
 
 export const projectsData = [
   {
     id: 1,
+    image: RumatKita,
+    title: "Rumatkita PWA",
+    category: "web",
+    url: "https://rumatkita.com",
+  },
+  {
+    id: 2,
+    image: Rumatku,
+    title: "Rumatku Absensi PWA",
+    category: "web",
+  },
+
+  {
+    id: 3,
+    image: Rosy,
+    title: "ROSY(Rumat Online System)",
+    category: "web",
+    url: "https://rosy.co.id",
+  },
+  {
+    id: 4,
     image: Klaklik,
     title: "Klaklik",
     category: "web",
     url: "https://www.klaklik.com/",
   },
   {
-    id: 2,
+    id: 5,
     image: Dashboard,
     title: "Creator Dashboard(Klaklik)",
     category: "web",
     url: "https://creator.klaklik.com/",
   },
   {
-    id: 3,
+    id: 6,
     image: CreatorForum,
     title: "Creator Forum(Klaklik)",
     category: "web",
     url: "https://creatorforum.klaklik.com/",
   },
   {
-    id: 4,
+    id: 7,
     image: Florist,
     title: "Florist Bouquet E-Commerce",
     category: "web",
   },
   {
-    id: 5,
+    id: 8,
     image: Burane,
     title: "Burane456 Coffee",
     category: "web",
   },
   {
-    id: 6,
-    image: Maltimart,
-    title: "Maltimart E-Commerce",
-    category: "web",
-  },
-  {
-    id: 7,
+    id: 9,
     image: TMDB,
     title: "Try Movie with TMDB API",
     category: "web",
   },
   {
-    id: 8,
+    id: 10,
     image: Kursus,
     title: "Website Kursus",
     category: "web",
